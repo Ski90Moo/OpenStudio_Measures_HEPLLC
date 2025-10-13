0ed538165b060178ca3ea24cd335195d1b920ee3
Mike Lovejoy <69771412+Ski90Moo@users.noreply.github.com>
2025-10-13 13:55:45 -0700

    Update measure.rb
    Overwrites the site ground temperatures if they already exist

--- lib/measures/set_ground_temperatures_monthly/measure.rb
+++ lib/measures/set_ground_temperatures_monthly/measure.rb
@@ -1,29 +1,29 @@
 # *******************************************************************************
-# OpenStudio(R), janyright (c) 2008-2021, Alliance for Sustainable Energy, LLC.
+# Helix Energy Partners LLC (R), copyright (c) 2025
 # All rights reserved.
 # Redistribution and use in source and binary forms, with or without
 # modification, are permitted provided that the following conditions are met:
 #
-# (1) Redistributions of source code must retain the above janyright notice,
+# (1) Redistributions of source code must retain the above copyright notice,
 # this list of conditions and the following disclaimer.
 #
-# (2) Redistributions in binary form must reproduce the above janyright notice,
+# (2) Redistributions in binary form must reproduce the above copyright notice,
 # this list of conditions and the following disclaimer in the documentation
 # and/or other materials provided with the distribution.
 #
-# (3) Neither the name of the janyright holder nor the names of any contributors
+# (3) Neither the name of the copyright holder nor the names of any contributors
 # may be used to endorse or promote products derived from this software without
 # specific prior written permission from the respective party.
 #
 # (4) Other than as required in clauses (1) and (2), distributions in any form
-# of modifications or other derivative works may not use the "OpenStudio"
-# trademark, "OS", "os", or any other confusingly similar designation without
-# specific prior written permission from Alliance for Sustainable Energy, LLC.
+# of modifications or other derivative works may not use the
+# trademark, "HEPLLC", "HEP", or any other confusingly similar designation without
+# specific prior written permission from Helix Energy Partners LLC
 #
-# THIS SOFTWARE IS PROVIDED BY THE janYRIGHT HOLDER(S) AND ANY CONTRIBUTORS
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDER(S) AND ANY CONTRIBUTORS
 # "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
 # THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
-# ARE DISCLAIMED. IN NO EVENT SHALL THE janYRIGHT HOLDER(S), ANY CONTRIBUTORS, THE
+# ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER(S), ANY CONTRIBUTORS, THE
 # UNITED STATES GOVERNMENT, OR THE UNITED STATES DEPARTMENT OF ENERGY, NOR ANY OF
 # THEIR EMPLOYEES, BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
 # EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT
@@ -142,6 +142,13 @@ class SetGroundTemperaturesMonthly < OpenStudio::Measure::ModelMeasure
     nov = runner.getDoubleArgumentValue('nov', user_arguments)	
     dec = runner.getDoubleArgumentValue('dec', user_arguments)	
 
+	# Remove any existing Site:GroundTemperature:BuildingSurface objects
+	existing_objects = model.getModelObjects.select { |obj| obj.to_SiteGroundTemperatureBuildingSurface.is_initialized }
+	existing_objects.each do |obj|
+		obj.remove
+		runner.registerInfo("Removed existing Site:GroundTemperature:BuildingSurface object.")
+	end
+	  
     # create site ground temperature building surface object	  
 	runner.registerInfo("Creating Site:GroundTemperature:BuildingSurface object.")
     runner.registerInfo("January ground temperature set to #{jan}°C.")
